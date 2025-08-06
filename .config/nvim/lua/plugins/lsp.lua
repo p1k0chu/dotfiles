ff6ac0a8ebb37ed972df33f89184db42ecb17ca9
p1k0chu <anatoliyid0207@gmail.com>
2025-08-06 13:18:12 +0300

    nvim: disable asm_lsp
    (its not good)

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,7 +6,7 @@ return {
             vim.lsp.enable('lua_ls')
             vim.lsp.enable('rust_analyzer')
             vim.lsp.enable('clangd')
-            vim.lsp.enable('asm_lsp')
+            -- vim.lsp.enable('asm_lsp')
         end
     },
     {
