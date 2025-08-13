de3e7b8c7f9bf749ff5c3ca9c0fe43471eedf7e0
p1k0chu <anatoliyid0207@gmail.com>
2025-08-13 10:12:04 +0300

    add overseer keybind to rerun last task

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -3,8 +3,20 @@ return {
     config = function()
         require("overseer").setup {}
 
+        -- command to rerun last task
+        vim.api.nvim_create_user_command("OverseerRestartLast", function()
+            local overseer = require("overseer")
+            local tasks = overseer.list_tasks({ recent_first = true })
+            if vim.tbl_isempty(tasks) then
+                vim.notify("No tasks found", vim.log.levels.WARN)
+            else
+                overseer.run_action(tasks[1], "restart")
+            end
+        end, {})
+
         vim.keymap.set("n", "<leader>rc", ":OverseerRunCmd<CR>", { desc = "[R]un [C]ommand" })
         vim.keymap.set("n", "<leader>rt", ":OverseerToggle<CR>", { desc = "[R]unner [T]oggle" })
-        vim.keymap.set("n", "<leader>rr", ":OverseerRun<CR>", {desc = "Overseer [R]un"})
+        vim.keymap.set("n", "<leader>rr", ":OverseerRun<CR>", { desc = "Overseer [R]un" })
+        vim.keymap.set("n", "<leader>rl", ":OverseerRestartLast<CR>", { desc = "Overseer [R]estart [L]ast"})
     end
 }
