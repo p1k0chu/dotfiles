3cf9c36ab16353d7ed55c5f59aa801d016d124c2
p1k0chu <anatoliyid0207@gmail.com>
2025-08-16 16:08:10 +0300

    hardcode floating kitty size
    weird height so its aligned with the cell size (no gap in the end)

--- scripts/kitty_float_maybe.sh
+++ scripts/kitty_float_maybe.sh
@@ -4,7 +4,7 @@
 count=$(hyprctl clients -j | jq '[.[] | select(.workspace.id == '"$(hyprctl activeworkspace -j | jq .id)"')] | length')
 
 if (( count > 0 )); then
-    hyprctl dispatch exec "[float; size 50% 50%] kitty $*"
+    hyprctl dispatch exec "[float; size 1000 588] kitty $*"
 else
     hyprctl dispatch exec "kitty $*"
 fi
